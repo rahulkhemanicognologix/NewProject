afab59bd9772e4f5539ffb995658aa375f533d74
RahulKhemani <rahulkhemani.it@gmail.com>
2021-10-07 13:21:47 +0530

    Changes

--- src/main/java/Learning/Queue.java
+++ src/main/java/Learning/Queue.java
@@ -20,4 +20,17 @@ public class Queue {
             System.out.print(queue[front+i]+ " ");
         }
     }
-}
+        //public class Run {
+        public static void main(String[] args) {
+            Queue r = new Queue();
+            r.enqueue(7);
+            r.enqueue(5);
+            r.enqueue(6);
+            r.enqueue(3);
+            r.dequeue();
+            r.dequeue();
+            r.show();
+
+        }
+    }
+
